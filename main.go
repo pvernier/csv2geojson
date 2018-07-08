d3e160c015f7508ab5487cac6f139f6ac948cd41
pvernier <pvernier82@gmail.com>
2018-07-09 00:29:42 +0400

    Modified usage and usage text

--- main.go
+++ main.go
@@ -17,20 +17,35 @@ import (
 )
 
 func main() {
-	csvFile := flag.String("in", "", "Input CSV (file or URL)")
+
 	colLong := flag.String("long", "", "Name of the column containing the longitude coordinates. If not provided, will try to guess")
 	colLat := flag.String("lat", "", "Name of the column containing the latitude coordinates. If not provided, will try to guess")
 	delimiter := flag.String("delimiter", ",", "Delimiter character")
-	jsonFile := flag.String("out", "", "Output GeoJSON file (extension will be added if omitted)")
 	keep := flag.String("keep", "n", "(y/n) If set to 'y' and the input CSV is an URL, keep the input CSV file on disk")
+
+	flag.Usage = func() {
+		help := "\nOptions:\n" + "  -" + flag.CommandLine.Lookup("delimiter").Name + ": " + flag.CommandLine.Lookup("delimiter").Usage + " (default \"" + flag.CommandLine.Lookup("delimiter").DefValue + "\")" + "\n"
+		help += "  -" + flag.CommandLine.Lookup("long").Name + ":      " + flag.CommandLine.Lookup("long").Usage + "\n"
+		help += "  -" + flag.CommandLine.Lookup("lat").Name + ":       " + flag.CommandLine.Lookup("lat").Usage + "\n"
+		help += "  -" + flag.CommandLine.Lookup("keep").Name + ":      " + flag.CommandLine.Lookup("keep").Usage + " (default \"" + flag.CommandLine.Lookup("keep").DefValue + "\")" + "\n"
+		fmt.Fprintf(os.Stderr, "Usage: %s [-options] <input> [output]\n%s", os.Args[0], help)
+	}
 
 	flag.Parse()
 
-	var r io.ReadCloser
+	var csvFile, jsonFile string
 
-	if *csvFile == "" {
-		fmt.Println("Error: You need to specify a CSV file. Use the '-in' option. To consult the help, use '-h'.")
+	if len(flag.Args()) == 0 {
+		fmt.Println("Error: You need to specify a CSV file. To consult the help, use '-h'.")
 		os.Exit(1)
+	} else if len(flag.Args()) > 2 {
+		fmt.Println("Error: You can only specify 2 arguments. To consult the help, use '-h'.")
+		os.Exit(1)
+	} else {
+		csvFile = flag.Args()[0]
+		if len(flag.Args()) == 2 {
+			jsonFile = flag.Args()[1]
+		}
 	}
 
 	*delimiter = strings.Trim(*delimiter, "'")
@@ -41,23 +56,25 @@ func main() {
 		newDelimiter = []rune(*delimiter)[0]
 	}
 
+	var r io.ReadCloser
+
 	// If the input CSV is a URL
-	if isValidURL(*csvFile) {
-		resp, err := http.Get(*csvFile)
+	if isValidURL(csvFile) {
+		resp, err := http.Get(csvFile)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: Couldn't access the URL: %s.\n", *csvFile)
+			fmt.Fprintf(os.Stderr, "Error: Couldn't access the URL: %s.\n", csvFile)
 			os.Exit(1)
 		}
 		defer resp.Body.Close()
 		if strings.ToLower(*keep) == "y" || strings.ToLower(*keep) == "yes" {
-			parts := strings.Split(*csvFile, "/")
+			parts := strings.Split(csvFile, "/")
 			newFile, err := os.Create(parts[len(parts)-1])
 			_, err = io.Copy(newFile, resp.Body)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error: Couldn't save the CSV file: %s to disk.\n", *csvFile)
+				fmt.Fprintf(os.Stderr, "Error: Couldn't save the CSV file: %s to disk.\n", csvFile)
 			}
-			*csvFile = parts[len(parts)-1]
-			r = readFile(*csvFile)
+			csvFile = parts[len(parts)-1]
+			r = readFile(csvFile)
 			defer r.Close()
 		} else {
 			r = resp.Body
@@ -67,11 +84,11 @@ func main() {
 		if strings.ToLower(*keep) == "y" || strings.ToLower(*keep) == "yes" {
 			fmt.Println("Info: The option '-keep' is only considered when the input file is an URL.")
 		}
-		r = readFile(*csvFile)
+		r = readFile(csvFile)
 		defer r.Close()
 	}
 
-	convert(r, *csvFile, *colLong, *colLat, *jsonFile, newDelimiter)
+	convert(r, csvFile, *colLong, *colLat, jsonFile, newDelimiter)
 }
 
 // readFile opens a file and returns a *File object
